fbcec5ef84f4df51e8692c4ddbd568aa45f44797
Lars Kiesow <lkiesow@uos.de>
2019-04-17 16:42:22 +0200

    /16 test

--- go-scan-http.go
+++ go-scan-http.go
@@ -8,6 +8,7 @@ import "time"
 var REQ_BEGIN []byte = []byte("HEAD / HTTP/1.1\r\nHost:")
 var REQ_END []byte = []byte("\r\n\r\n")
 const TIMEOUT time.Duration = 3 * time.Second
+const THREADS int = 512
 
 func probe(addr string, ret chan string) {
     d := net.Dialer{Timeout: TIMEOUT}
@@ -16,6 +17,7 @@ func probe(addr string, ret chan string) {
         ret <- fmt.Sprintf("%s", err)
         return
     }
+    defer conn.Close()
     conn.SetDeadline(time.Now().Add(TIMEOUT))
     conn.Write(REQ_BEGIN)
     conn.Write([]byte(addr))
@@ -33,25 +35,34 @@ func probe(addr string, ret chan string) {
             break
         }
     }
-    conn.Close()
     ret <- header
 }
 
 func main() {
-    queue := make(chan string, 256)
+    queue := make(chan string, THREADS)
+    queue := make(chan bool, THREADS)
     // https://ipinfo.io/AS680/131.173.0.0/16
-    for i := 0; i < 256; i++ {
-        addr := fmt.Sprintf("131.173.168.%d:80", i)
-        //addr := fmt.Sprintf("141.100.10.%d:80", i)
-        //addr := fmt.Sprintf("52.73.210.%d:80", i)
-        //addr := fmt.Sprintf("192.168.1.%d:80", i)
-        go probe(addr, queue)
-        go probe(addr + "80", queue)
-    }
-    for i := 0; i < 256 * 2; i++ {
-        addr := <-queue
-        if addr[0] == '1' {
-            fmt.Println(addr)
+    init := THREADS
+    for i := 168; i < 168 + 26; i++ {
+        for j := 0; j < 256; j++ {
+            addr := fmt.Sprintf("131.173.%d.%d:80", i, j)
+            //addr := fmt.Sprintf("131.173.168.%d:80", i)
+            //addr := fmt.Sprintf("141.100.10.%d:80", i)
+            //addr := fmt.Sprintf("52.73.210.%d:80", i)
+            //addr := fmt.Sprintf("192.168.1.%d:80", i)
+            go probe(addr, queue)
+            go probe(addr + "80", queue)
+            fmt.Println(init, i, j)
+            if init > 0 {
+                init--
+            } else {
+                result := <-queue
+                fmt.Println(result)
+            }
         }
+    }
+    for i := 0; i < THREADS * 2; i++ {
+        addr := <-queue
+        fmt.Println(addr)
     }
 }
